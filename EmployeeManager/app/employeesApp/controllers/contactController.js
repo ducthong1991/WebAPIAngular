dd93c0b71a617aeabda4fc969ca25f6024c4bda3
DESKTOP-137CLT9\Dell <ducthong.1991@gmail.com>
2017-01-08 16:23:38 +0700

    no message

---
+++ EmployeeManager/app/employeesApp/controllers/contactController.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    var injectParams = ['$scope'];
+
+    var contactController = function ($scope) {
+
+        $scope.$watch('aModel', function (newValue, oldValue) {
+            alert(newValue + '222-333' + oldValue);
+        });
+    };
+
+    contactController.$inject = injectParams;
+    angular.module('employeesApp').controller('contactController', contactController);
+})();
